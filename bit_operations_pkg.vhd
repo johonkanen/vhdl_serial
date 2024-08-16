5940cc9d8df24ca306cfeac8978ed3b3d0d3a096
Jari Honkanen <jaarhonka@yahoo.com>
2024-08-16 23:31:57 +0300

    minor visual clean up

--- bit_operations_pkg.vhd
+++ bit_operations_pkg.vhd
@@ -3,18 +3,19 @@ library ieee;
     use ieee.numeric_std.all;
 
 package bit_operations_pkg is
-
+-----------------------------------------------
     procedure left_shift (
         signal shift_register : inout std_logic_vector; input : in std_logic);
-
+-----------------------------------------------
     function get_first_bit ( input : std_logic_vector )
         return std_logic;
+-----------------------------------------------
 
 end package bit_operations_pkg;
 
 package body bit_operations_pkg is
 
-    -- todo, move somewhere else
+-----------------------------------------------
     procedure left_shift
     (
         signal shift_register : inout std_logic_vector; input : in std_logic
@@ -24,6 +25,7 @@ package body bit_operations_pkg is
         shift_register <= shift_register(shift_register'left-1 downto 0 ) & input;
         
     end left_shift;
+-----------------------------------------------
 
     function get_first_bit
     (
@@ -34,6 +36,6 @@ package body bit_operations_pkg is
     begin
         return input(input'left);
     end get_first_bit;
-
+-----------------------------------------------
 
 end package body bit_operations_pkg;

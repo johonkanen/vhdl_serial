fdb701def04260b312ed1bb644251429e01ed472
Jari Honkanen <jaarhonka@yahoo.com>
2024-09-06 07:07:16 +0300

    move package instantiations out of ads7056 package

--- source/ads7056/ads7056_pkg.vhd
+++ source/ads7056/ads7056_pkg.vhd
@@ -2,11 +2,13 @@ library ieee;
     use ieee.std_logic_1164.all;
     use ieee.numeric_std.all;
 
-package spi_adc_type_pkg is
+package spi_adc_type_generic_pkg is
+    generic(package spi_adc_clk_div_pkg is new work.clock_divider_generic_pkg generic map(<>));
+    use spi_adc_clk_div_pkg.all;
 
     type spiadc_record is record
-        clock_divider        : clock_divider_record;
-        data_capture_counter : clock_divider_record;
+        clock_divider        : spi_adc_clk_div_pkg.clock_divider_record;
+        data_capture_counter : spi_adc_clk_div_pkg.clock_divider_record;
         data_capture_delay   : natural range 0 to 7;
         state                : natural range 0 to 7;
         conversion_requested : boolean;
@@ -15,29 +17,66 @@ package spi_adc_type_pkg is
         is_ready             : boolean;
     end record;
 
-end spi_adc_type_pkg;
+    procedure create_adc_state_machine (
+        signal self : inout spiadc_record);
+
+end spi_adc_type_generic_pkg;
+
+package body spi_adc_type_generic_pkg is
+
+    procedure create_adc_state_machine
+    (
+        signal self : inout spiadc_record
+    ) 
+    is
+        constant wait_for_init : natural := 0;
+        constant initializing  : natural := 1;
+        constant ready         : natural := 2;
+        constant converting    : natural := 3;
+    begin
+        CASE self.state is 
+            WHEN wait_for_init =>
+                if self.conversion_requested then
+                    request_number_of_clock_pulses(self.clock_divider, 24);
+                    self.state <= 1;
+                end if;
+            WHEN initializing  =>
+                if clock_divider_is_ready(self.clock_divider) then
+                    self.state <= 2;
+                end if;
+            WHEN ready =>
+                if self.conversion_requested then
+                    self.data_capture_delay <= 3;
+                    request_number_of_clock_pulses(self.clock_divider, 18);
+                    request_number_of_clock_pulses(self.data_capture_counter, 18);
+                    self.state <= 3;
+                end if;
+            WHEN converting =>
+                if clock_divider_is_ready(self.data_capture_counter) then
+                    self.state <= 2;
+                end if;
+            WHEN others =>
+        end CASE;
+    end create_adc_state_machine;
+
+end package body spi_adc_type_generic_pkg;
 
 -----------------------------------------
 library ieee;
     use ieee.std_logic_1164.all;
     use ieee.numeric_std.all;
 
-    use work.spi_adc_type_pkg.all;
-
 package ads7056_generic_pkg is
     generic(
             idle_state_number : natural := 0;
-            g_count_max : natural range 0 to 127 := 3);
-
-    package ads7056_clock_divider_pkg is new work.clock_divider_generic_pkg generic map(g_count_max => g_count_max);
+            package ads7056_clock_divider_pkg is new work.clock_divider_generic_pkg generic map(<>);
+            package ads7056_type_pkg is new work.spi_adc_type_generic_pkg generic map(<>)
+        );
     use ads7056_clock_divider_pkg.all;
+    use ads7056_type_pkg.all;
 
-    alias ads7056_record is spi_adc_type_pkg.spiadc_record;
-
-    /* type ads7056_states is (wait_for_init, initializing, ready, converting); */
-    /* signal state : ads7056_states := wait_for_init; */
-
-
+-------------------------------------------------------------------
+    alias ads7056_record is ads7056_type_pkg.spiadc_record;
     constant init_ads7056 : ads7056_record := (init_clock_divider,init_clock_divider,4, idle_state_number, false, (others => '0'), (others => '0'), false);
 
 -------------------------------------------------------------------
@@ -65,35 +104,6 @@ end package ads7056_generic_pkg;
 
 package body ads7056_generic_pkg is
 
-    procedure create_adc_state_machine
-    (
-        signal self : inout ads7056_record
-    ) is
-    begin
-        CASE self.state is 
-            WHEN 0 =>
-                if self.conversion_requested then
-                    request_number_of_clock_pulses(self.clock_divider, 24);
-                    self.state <= 1;
-                end if;
-            WHEN 1  =>
-                if clock_divider_is_ready(self.clock_divider) then
-                    self.state <= 2;
-                end if;
-            WHEN 2 =>
-                if self.conversion_requested then
-                    self.data_capture_delay <= 3;
-                    request_number_of_clock_pulses(self.clock_divider, 18);
-                    request_number_of_clock_pulses(self.data_capture_counter, 18);
-                    self.state <= 3;
-                end if;
-            WHEN 3 =>
-                if clock_divider_is_ready(self.data_capture_counter) then
-                    self.state <= 2;
-                end if;
-            WHEN others =>
-        end CASE;
-    end create_adc_state_machine;
 
 -------------------------------------------------------------------
     procedure create_ads7056_driver
@@ -178,4 +188,10 @@ end package body ads7056_generic_pkg;
 -------------------------------------------------------------------
 -- default instantiation, TODO : remove 
 
-package ads7056_pkg is new work.ads7056_generic_pkg;
+package test_clock_divider_pkg is new work.clock_divider_generic_pkg generic map(g_count_max => 3);
+package test_ads7056_type_pkg is new work.spi_adc_type_generic_pkg generic map(work.test_clock_divider_pkg);
+
+package ads7056_pkg is new work.ads7056_generic_pkg 
+    generic map(
+            ads7056_clock_divider_pkg => work.test_clock_divider_pkg,
+            ads7056_type_pkg          => work.test_ads7056_type_pkg);
